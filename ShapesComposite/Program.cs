78683d4e3e0fc535bba1c93399563d828e0b71d3
cristianagrigoriu <cristiana.grigoriu@gmail.com>
2014-07-11 11:28:45 +0300

    composite shape

--- ShapesComposite/Program.cs
+++ ShapesComposite/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Shapes;
 
 namespace ShapesComposite
 {
@@ -10,6 +11,20 @@ namespace ShapesComposite
     {
         static void Main(string[] args)
         {
+            var shapeContainer1 = new ShapeContainer();
+            var shapeContainer2 = new ShapeContainer();
+            var shapeContainer3 = new ShapeContainer();
+
+            shapeContainer1.Add(new Rectangle("purple", 20, 9));
+            shapeContainer1.Add(new Triangle(2, 2));
+
+            shapeContainer2.Add(new Triangle(6, 10));
+
+            shapeContainer3.AddRange(shapeContainer1, shapeContainer2);
+
+            Console.WriteLine(shapeContainer3.ToString());
+            Console.WriteLine("Total Area = {0}", shapeContainer3.GetArea());
+            
         }
     }
 }
--- ShapesComposite/ShapeContainer.cs
+++ ShapesComposite/ShapeContainer.cs
@@ -1,12 +1,61 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Shapes;
 
 namespace ShapesComposite
 {
-    class ShapeContainer
+    class ShapeContainer : IShape
     {
+        private List<IShape> childShapes;
+        public string Colour { get; set; }
+
+        public ShapeContainer()
+        {
+            childShapes = new List<IShape>();
+        }
+
+        //how to override it?
+        public double GetArea()
+        {
+            double result = 0;
+
+            foreach (IShape shape in this.childShapes)
+            {
+                result += shape.GetArea();
+            }
+
+            return result;
+        }
+
+        public string ToString()
+        {
+            string result = "";
+
+            foreach (IShape shape in this.childShapes)
+            {
+                result += shape.ToString();
+            }
+
+            return result;
+        }
+
+        public void Add(IShape newShape)
+        {
+            this.childShapes.Add(newShape);
+        }
+
+        public void AddRange(params IShape[] newShapes)
+        {
+            this.childShapes.AddRange(newShapes);
+        }
+
+        public void Remove(IShape shapeToRemove)
+        {
+            this.childShapes.Remove(shapeToRemove);
+        }
     }
 }
