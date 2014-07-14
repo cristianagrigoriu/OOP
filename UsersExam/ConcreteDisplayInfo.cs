385a8bceb5d6ffd9d1f6f994fb54e1b2229d5817
cristianagrigoriu <cristiana.grigoriu@gmail.com>
2014-07-14 17:26:00 +0300

    start abstract factory for display info

---
+++ UsersExam/ConcreteDisplayInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsersExam
+{
+    class ConcreteDisplayInfo : IAbstractDisplayInfo
+    {
+        public IDisplayInfo CreateDisplayInfoToFile()
+        {
+            return new DisplayToFile();
+        }
+
+        public IDisplayInfo CreateDisplayInfoToConsole()
+        {
+            return new DisplayToConsole();
+        }
+    }
+}
---
+++ UsersExam/IAbstractDisplayInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsersExam
+{
+    public interface IAbstractDisplayInfo
+    {
+        IDisplayInfo CreateDisplayInfoToFile();
+        IDisplayInfo CreateDisplayInfoToConsole();
+    }
+}
